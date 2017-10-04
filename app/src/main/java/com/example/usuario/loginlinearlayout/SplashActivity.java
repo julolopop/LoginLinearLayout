5068c622fb75cdbbb727a41222ed85b4fb879a6a
juanma <julolopop@gmail.com>
2017-10-04 14:45:14 +0200

    Proyecto Sign in

---
+++ app/src/main/java/com/example/usuario/loginlinearlayout/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.usuario.loginlinearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Juan Manuel Diaz Ortiz
+ * @version v0.1
+ * @use
+ *
+ * */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
