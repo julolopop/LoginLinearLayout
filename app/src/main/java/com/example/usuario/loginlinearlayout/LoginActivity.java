74654805462abc9d4abf83bf8d34bd1d45cafc8a
juanma <julolopop@gmail.com>
2017-10-04 14:45:57 +0200

    Proyecto Sign in resubido

---
+++ app/src/main/java/com/example/usuario/loginlinearlayout/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.usuario.loginlinearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @author Juan Manuel Diaz Ortiz
+ * @version v0.1
+ * @use
+ *
+ * */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
