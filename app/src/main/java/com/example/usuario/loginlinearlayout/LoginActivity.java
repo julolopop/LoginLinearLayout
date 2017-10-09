016e85171ddafdcb23763573cfb631fd9ee230ff
juanma <julolopop@gmail.com>
2017-10-09 12:49:10 +0200

    Subrayado

--- app/src/main/java/com/example/usuario/loginlinearlayout/LoginActivity.java
+++ app/src/main/java/com/example/usuario/loginlinearlayout/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.usuario.loginlinearlayout;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 /**
  * @author Juan Manuel Diaz Ortiz
  * @version v0.1
@@ -10,9 +13,14 @@ import android.os.Bundle;
  * */
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView txvSingUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.txvSingUp.setPaintFlags(this.txvSingUp.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
     }
 }
--- app/src/main/java/com/example/usuario/loginlinearlayout/SplashActivity.java
+++ app/src/main/java/com/example/usuario/loginlinearlayout/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.example.usuario.loginlinearlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * @author Juan Manuel Diaz Ortiz
@@ -11,9 +15,31 @@ import android.os.Bundle;
  * */
 public class SplashActivity extends AppCompatActivity {
 
+    // Set the duration of the splash screen
+    private static final long SPLASH_SCREEN_DELAY = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        SplashActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
 }
